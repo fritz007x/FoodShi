24025f04efa5f3c62e6c9788a219b42702a02b4c
fritz007x <fritz007x@gmail.com>
2023-12-09 23:19:14 -0500

    Refactor and improve contract logic

--- contracts/MedalNFT.sol
+++ contracts/MedalNFT.sol
@@ -1,100 +1,85 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "hardhat/console.sol";
 
 error ERC721Metadata__URI_QueryFor_NonExistentToken();
 
-contract MedalNFT is
-    ERC721,
-    ERC721URIStorage,
-    Ownable //Dynamic NFT
-{
-    using Counters for Counters.Counter;
+contract MedalNFT is ERC721, ERC721URIStorage, Ownable {
+    // using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    // Counters.Counter private _tokenIdCounter;
 
-    event CreatedNFT(uint256 indexed tokenId, int256 indexed medalValue); //Elaborate on medal value
+    uint private _tokenIdCounter;
 
-    // event TransferedNFT(
-    //     address indexed recipient,
-    //     address indexed nftAddress,
-    //     uint256 indexed tokenId
-    );
+    event TokenMinted(uint256 indexed tokenId, address to); //int256 medalValue
+    event TokenURIUpdated(uint256 tokenId, string uri);
 
-    // Metadata information for each stage of the NFT on IPFS.
-    string[] IpfsUri = [
-        "",
-        "",
-        ""
-        // "https://ipfs.io/ipfs/QmYaTsyxTDnrG4toc8721w62rL4ZBKXQTGj9c9Rpdrntou/seed.json",
-        // "https://ipfs.io/ipfs/QmYaTsyxTDnrG4toc8721w62rL4ZBKXQTGj9c9Rpdrntou/purple-sprout.json",
-        // "https://ipfs.io/ipfs/QmYaTsyxTDnrG4toc8721w62rL4ZBKXQTGj9c9Rpdrntou/purple-blooms.json"
-    ];
+    //New URIs can be added in the future
+    string[] public tokenURIs = ["", "", ""];
 
-    constructor() ERC721("Dynamic Medal NFT", "DMN") {
-        _tokenIdCounter._value = 0;
+    address public immutable medalLevelContract;
+
+    constructor(address _medalLevelContract) ERC721("Medal NFT", "DMN") {
+        _tokenIdCounter = 0;
+        medalLevelContract = _medalLevelContract;
     }
 
     //This function is executed by the donor when he has achieved a goal for it
     //medalValue is just for tracking the type of medal awarded. Consider not to use it
 
-    function awardMedal(
-        int256 medalValue,
-        address donnorAddress
-    ) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(donnorAddress, tokenId);
-        _setTokenURI(tokenId, IpfsUri[0]); //The user first mint the initial medal
+    function mint(
+        uint256 tokenId, //medalValue,
+        address account,
+        string memory newURI
+    ) public {
+        require(msg.sender == medalLevelContract, "Direct mint invalid");
+        // uint256 tokenId = _tokenIdCounter.current();
+        // _tokenIdCounter.increment();
+        ++_tokenIdCounter;
+        _safeMint(account, tokenId);
+        _setTokenURI(tokenId, newURI); //The user first mint the initial medal
 
-        // emit CreatedNFT(tokenId, medalValue);
+        emit TokenMinted(tokenId, account);
+    }
+
+    /**
+     * @dev Updates the URI of an existing token.
+     * Only the FetchmedalLevel contract can call this function.
+     * @param tokenId The ID of the token.
+     * @param newUri The new URI of the token.
+     */
+    function setURI(uint256 tokenId, string memory newUri) public {
+        //onlyOwner
+        require(
+            msg.sender == medalLevelContract,
+            "URI can be only be updated by FetchmedalLevel contract"
+        );
+
+        _setTokenURI(tokenId, newUri);
+        emit TokenURIUpdated(tokenId, newUri); //, block.timestamp
     }
 
     //This function set the token URI of the same NFT to a new value
-    function upgradeMedal(uint256 _tokenId) public {
-        uint256 medalQty = IpfsUri.length;
+    // function upgradeMedal(uint256 _tokenId) public {
+    //     uint256 medalQty = IpfsUri.length;
 
-        if (medalStage(_tokenId) >= medalQty) {
-            return;
-        }
-        // Get the current stage of the medal and increment it
-        uint256 newVal = medalStage(_tokenId) + 1;
-        // store the new URI
-        string memory newUri = IpfsUri[newVal];
-        // Update the URI
-        _setTokenURI(_tokenId, newUri);
-    }
-
-    //This function returns the current stage of the medal
-    function medalStage(uint256 _tokenId) public view returns (uint256) {
-        string memory _uri = tokenURI(_tokenId);
-        // 1st medal
-        if (compareStrings(_uri, IpfsUri[0])) {
-            return 0;
-        }
-        // 2nd medal
-        if (compareStrings(_uri, IpfsUri[1])) {
-            return 1;
-        }
-        // 3rd medal
-        return 2; //This might be augmented
-    }
-
-    // helper function to compare strings
-    function compareStrings(
-        string memory a,
-        string memory b
-    ) public pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) ==
-            keccak256(abi.encodePacked((b))));
-    }
+    //     if (medalStage(_tokenId) >= medalQty) {
+    //         return;
+    //     }
+    //     // Get the current stage of the medal and increment it
+    //     uint256 newVal = medalStage(_tokenId) + 1;
+    //     // store the new URI
+    //     string memory newUri = IpfsUri[newVal];
+    //     // Update the URI
+    //     _setTokenURI(_tokenId, newUri);
+    // }
 
     // The following functions are overrides required by Solidity.
 
