4fe2e82a10ddcac5eaf65f5a69731e5190340245
fritz007x <fritz007x@gmail.com>
2023-12-10 23:07:33 -0500

    Refactor and improve contract functions

--- contracts/MedalNFT.sol
+++ contracts/MedalNFT.sol
@@ -5,18 +5,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "hardhat/console.sol";
 
-error ERC721Metadata__URI_QueryFor_NonExistentToken();
+// error ERC721Metadata__URI_QueryFor_NonExistentToken();
 
-contract MedalNFT is ERC721, ERC721URIStorage, Ownable {
-    // using Counters for Counters.Counter;
-
-    // Counters.Counter private _tokenIdCounter;
-
-    uint private _tokenIdCounter;
+contract MedalNFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
 
     event TokenMinted(uint256 indexed tokenId, address to); //int256 medalValue
     event TokenURIUpdated(uint256 tokenId, string uri);
@@ -26,45 +21,48 @@ contract MedalNFT is ERC721, ERC721URIStorage, Ownable {
 
     address public immutable medalLevelContract;
 
-    constructor(address _medalLevelContract) ERC721("Medal NFT", "DMN") {
-        _tokenIdCounter = 0;
+    constructor(address _medalLevelContract) ERC721("Medal NFT", "FMN") {
         medalLevelContract = _medalLevelContract;
+        _tokenIdCounter = 0;
     }
 
-    //This function is executed by the donor when he has achieved a goal for it
-    //medalValue is just for tracking the type of medal awarded. Consider not to use it
+    /**
+     * @dev Mints a token to the specified account.
+     * Only the Medal Level contract can call this function directly.
+     * @param account The account to mint the token to.
+     * @param medalLevel The ID of the token.
+    
+     */
 
-    function mint(
-        uint256 tokenId, //medalValue,
+    function awardMedal(
+        //uint256 tokenId, //medalValue,
         address account,
-        string memory newURI
+        uint256 medalLevel // string memory newURI
     ) public {
         require(msg.sender == medalLevelContract, "Direct mint invalid");
         // uint256 tokenId = _tokenIdCounter.current();
         // _tokenIdCounter.increment();
         ++_tokenIdCounter;
+        uint256 tokenId = _tokenIdCounter;
         _safeMint(account, tokenId);
-        _setTokenURI(tokenId, newURI); //The user first mint the initial medal
+        _setTokenURI(tokenId, tokenURIs[medalLevel - 1]); //At the beggining users have no medal (medal level=0)
 
         emit TokenMinted(tokenId, account);
     }
 
     /**
-     * @dev Updates the URI of an existing token.
-     * Only the FetchmedalLevel contract can call this function.
-     * @param tokenId The ID of the token.
-     * @param newUri The new URI of the token.
+     *
      */
-    function setURI(uint256 tokenId, string memory newUri) public {
-        //onlyOwner
-        require(
-            msg.sender == medalLevelContract,
-            "URI can be only be updated by FetchmedalLevel contract"
-        );
+    // function setURI(uint256 tokenId, string memory newUri) public {
+    //     //onlyOwner
+    //     require(
+    //         msg.sender == medalLevelContract,
+    //         "URI can be only be updated by FetchmedalLevel contract"
+    //     );
 
-        _setTokenURI(tokenId, newUri);
-        emit TokenURIUpdated(tokenId, newUri); //, block.timestamp
-    }
+    //     _setTokenURI(tokenId, newUri);
+    //     emit TokenURIUpdated(tokenId, newUri); //, block.timestamp
+    // }
 
     //This function set the token URI of the same NFT to a new value
     // function upgradeMedal(uint256 _tokenId) public {
@@ -83,15 +81,35 @@ contract MedalNFT is ERC721, ERC721URIStorage, Ownable {
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(
-        uint256 tokenId
-    ) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override(ERC721URIStorage) {
         super._burn(tokenId);
     }
 
     function tokenURI(
         uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    ) public view override(ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
+    }
+
+    // Getters
+    /**
+     * @dev Checks if a token with the given ID exists.
+     * @param tokenId The ID of the token.
+     * @return A boolean indicating whether the token exists.
+     */
+    function tokenExists(uint256 tokenId) public view returns (bool) {
+        return tokenId < _tokenIdCounter;
+    }
+
+    /**
+     * @dev Gets the total number of tokens created.
+     * @return The total number of tokens.
+     */
+    function getTotalTokens() public view returns (uint256) {
+        return _tokenIdCounter;
+    }
+
+    function getTokenURIs() public view returns (string[] memory) {
+        return tokenURIs;
     }
 }
